96e15e6ba9fb9f831a12ae7a2835225b4aee6c04
Sergeis87 <sergeis87@mail.ru>
2023-04-28 15:57:31 +0300

    1st commit

---
+++ src/test/java/GoogleTest.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class GoogleTest {
+    @Test
+    public void googleTest(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        WebElement findField = driver.findElement(By.xpath("//textarea[@title ='Поиск']"));
+        findField.sendKeys("Selenium");
+        findField.sendKeys(Keys.ENTER);
+        WebElement text = driver.findElement(By.xpath("//h3[text()='Selenium']"));
+        Assert.assertEquals(text.getText(), "Selenium");
+        driver.quit();
+    }
+}
